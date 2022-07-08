e2eec2d2d2ce32307abb2574d6dc4cb2565b8ef1
maniamartial <martialmania19@gmail.com>
2022-07-09 00:06:46 +0300

    Changes on conjs

--- wefruitfulserver/db/conn.js
+++ wefruitfulserver/db/conn.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongose');
+const mongoose = require('mongoose');
 
 const db = process.env.DATABASE;
-monngose.connect(db, {
+mongoose.connect(db, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
     console.log('Connection Successfull');
-}).catch(()=>{
+}).catch((e)=>{
     console.log(e);
 })
